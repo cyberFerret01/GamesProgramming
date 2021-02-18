662878aa70a56d458bd7b7311c4454fbb4c8f58b
cyberFerret01 <60542526+cyberFerret01@users.noreply.github.com>
2021-02-18 22:25:02 +0000

    second

---
+++ messingAround/Assets/WASDmove.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WASDmove : MonoBehaviour
+{
+    int speed = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+         
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += Vector3.forward * speed * Time.deltaTime;
+            
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += Vector3.back * speed * Time.deltaTime;
+        }
+        GameObject.Find("Box").transform.position = GameObject.Find("Camera").transform.position;
+    }
+}

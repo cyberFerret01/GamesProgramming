bcf8b7dd9ec66281ec2e555ee1c0d65a21af5ded
cyberFerret01 <60542526+cyberFerret01@users.noreply.github.com>
2021-02-22 20:27:19 +0000

    navMeshStarted

--- messingAround/Assets/patrol.cs
+++ messingAround/Assets/patrol.cs
@@ -15,4 +15,114 @@ public class patrol : MonoBehaviour
     {
         
     }
+
+    private Transform target;
+    private Quaternion _lookRotation;
+    private Vector3 _direction;
+    private byte partrol = 0;
+
+    int[] array = new int[] { 0, 1, 2, 3 };
+    // Update is called once per frame
+    Quaternion path(int[] route, string waypoints)
+    {
+        string partrolTarget = "bookcase" + array[partrol];
+        target = GameObject.Find(partrolTarget).transform;
+        Vector3 t = target.transform.position;
+        Vector3 s = transform.position;
+
+        if (Vector3.Distance(t, s) < 5)
+        {
+            partrol++;
+            if (partrol == array.Length) partrol = 0;
+        }
+
+        //find the vector pointing from our position to the target
+        _direction = (target.position - transform.position).normalized;
+
+        //create the rotation we need to be in to look at the target
+        return Quaternion.LookRotation(_direction);
+
+    }
+
+
+    }
+/*
+ * 
+ * Each enemy has a list of waypoints in their patrol
+ * Once they reach a waypoint it is iterated to the next point
+ * The funtion takes in the waypoint type (such as bookcase or cube) and the list used by the individual 
+ * outputs the direction the object needs to look at
+ * 
+ */
+
+/*
+ * 
+ * 
+ * using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr : MonoBehaviour
+{
+    public Rigidbody PlayerRB;
+    private Transform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerRB = GetComponent<Rigidbody>();
+        target = GameObject.Find("Capsule").transform;
+    }
+
+    // Update is called once per frame
+    private Quaternion _lookRotation;
+    private Vector3 _direction;
+    private byte partrol = 0;
+
+    int[] array = new int[] { 0, 1, 2, 3 };
+    // Update is called once per frame
+    void Update()
+    {
+        string partrolTarget = "bookcase" + array[partrol];
+        target = GameObject.Find(partrolTarget).transform;
+        Vector3 t =  target.transform.position;
+        Vector3 s = transform.position;
+
+        if (Vector3.Distance(t, s) < 5)
+        {
+            partrol++;
+            if (partrol == array.Length) partrol = 0;
+        }
+
+        //find the vector pointing from our position to the target
+        _direction = (target.position - transform.position).normalized;
+
+        //create the rotation we need to be in to look at the target
+        _lookRotation = Quaternion.LookRotation(_direction);
+        // StartCoroutine(move());
+
+        float step = 90; // calculate distance to move
+        transform.rotation = Quaternion.Slerp(transform.rotation, _lookRotation, Time.deltaTime * 10000);
+       StartCoroutine(move()); 
+
+       
+      
+    }
+
+
+
+IEnumerator move(){
+
+
+        transform.position += Vector3.up * 8 * Time.deltaTime;
+ 
+        transform.position += transform.forward *2 * Time.deltaTime;
+        transform.rotation = Quaternion.Euler(0, 0, 0);
+
+        if (Mathf.Approximately(PlayerRB.velocity.y, 0)) {
+            yield return new WaitForSeconds(1);
+        }
+    }
 }
+
+ * 
+ */
--- messingAround/Assets/scr.cs
+++ messingAround/Assets/scr.cs
@@ -1,65 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class scr : MonoBehaviour
 {
-    public Rigidbody PlayerRB;
-    private Transform target;
-    // Start is called before the first frame update
-    void Start()
-    {
-        PlayerRB = GetComponent<Rigidbody>();
-        target = GameObject.Find("Capsule").transform;
-    }
 
-    // Update is called once per frame
+    private Transform target;
     private Quaternion _lookRotation;
     private Vector3 _direction;
     private byte partrol = 0;
 
-    int[] array = new int[] { 0, 1, 2, 3 };
+    public Transform[] points;
+    private NavMeshAgent nav;
+    private int destPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nav = GetComponent<NavMeshAgent>();
+    }
+
     // Update is called once per frame
     void Update()
     {
+
+
+        if (!nav.pathPending && nav.remainingDistance < 5)
+            GoToNextPoint();
+
+
+    }
+
+
+    int[] array = new int[] { 0, 1, 2, 3 };
+
+
+        void GoToNextPoint(){
+        if (partrol > 3){
+
+            partrol = 0;
+        }
+
+
         string partrolTarget = "bookcase" + array[partrol];
         target = GameObject.Find(partrolTarget).transform;
-        Vector3 t =  target.transform.position;
-        Vector3 s = transform.position;
 
-        if (Vector3.Distance(t, s) < 5)
-        {
-            partrol++;
-            if (partrol == array.Length) partrol = 0;
-        }
-
-        //find the vector pointing from our position to the target
-        _direction = (target.position - transform.position).normalized;
-
-        //create the rotation we need to be in to look at the target
-        _lookRotation = Quaternion.LookRotation(_direction);
-        // StartCoroutine(move());
-
-        float step = 90; // calculate distance to move
-        transform.rotation = Quaternion.Slerp(transform.rotation, _lookRotation, Time.deltaTime * 10000);
-       StartCoroutine(move()); 
-
-       
-      
+         nav.destination = target.position;
+        partrol++;
     }
 
 
-
-IEnumerator move(){
-
-
-        transform.position += Vector3.up * 8 * Time.deltaTime;
- 
-        transform.position += transform.forward *2 * Time.deltaTime;
-        transform.rotation = Quaternion.Euler(0, 0, 0);
-
-        if (Mathf.Approximately(PlayerRB.velocity.y, 0)) {
-            yield return new WaitForSeconds(1);
-        }
-    }
 }
